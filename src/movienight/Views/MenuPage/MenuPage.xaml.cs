688ec172059a5df4104a82f8c086c3e2845b42fc
Hussain Abbasi <hnabbasi@outlook.com>
2020-10-28 17:45:10 -0500

    Starting

--- src/movienight/Views/MenuPage/MenuPage.xaml.cs
+++ src/movienight/Views/MenuPage/MenuPage.xaml.cs
@@ -1,37 +1,13 @@
-﻿using Xamarin.Forms;
-using Xamarin.Essentials;
-using System;
+﻿using System;
+using Xamarin.Forms;
 
 namespace MovieNight.Views.MenuPage
 {
     public partial class MenuPage : ContentPage
     {
-        private readonly ControlTemplate _horizontal = new HorizontalMenuTemplate();
-        private readonly ControlTemplate _vertical = new VerticalMenuTemplate();
-
         public MenuPage()
         {
             InitializeComponent();
-            DeviceDisplay_MainDisplayInfoChanged(this, new DisplayInfoChangedEventArgs(DeviceDisplay.MainDisplayInfo));
-        }
-
-        private void DeviceDisplay_MainDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
-        {
-            ControlTemplate = e.DisplayInfo.Orientation == DisplayOrientation.Portrait
-                ? _vertical
-                : _horizontal;
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            DeviceDisplay.MainDisplayInfoChanged += DeviceDisplay_MainDisplayInfoChanged;
-        }
-
-        protected override void OnDisappearing()
-        {
-            base.OnDisappearing();
-            DeviceDisplay.MainDisplayInfoChanged -= DeviceDisplay_MainDisplayInfoChanged;
         }
     }
 }
